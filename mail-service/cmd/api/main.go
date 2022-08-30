067b08b7a877a05aed18096ddd4742cff6559312
lightsaid <lightsaid@163.com>
2022-08-30 17:23:06 +0800

    feat：邮件服务

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+// NOTE:
+// 用于发送电子邮件的 Golang 包。支持保持连接、TLS 和 SSL。易于批量 SMTP。
+// go get github.com/xhit/go-simple-mail/v2
+
+// golang中html邮件的内联样式
+// go get github.com/vanng822/go-premailer
+
 const webPort = ":80"
 type Config struct{}
 
